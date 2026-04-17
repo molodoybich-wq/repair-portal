ef1e8588f5ca870e9503edd7f0e9e6e6d7e8216a
molodoybich-wq <molodoybich@gmail.com>
2026-04-17 16:32:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // ← ВОТ ЭТО ВАЖНО ДОБАВИТЬ
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
